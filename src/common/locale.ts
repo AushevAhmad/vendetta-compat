5bf1654ccf1df03cbd1e8d5b970ad7c1dc3377fe
wont.stream <drns.pw@icloud.com>
2024-07-22 04:42:07 -0400

    update

--- src/common/locale.ts
+++ src/common/locale.ts
@@ -1,108 +1,58 @@
 import { getIDByName } from "enmity/api/assets";
-import Manifest from "../../manifest.json"
+import Manifest from "../../manifest.json";
 
 export default {
-    errors: {
-        unload: [
-            "Erdetta whedetta trydetta todetta unlodetta Vendetta",
-            "Error when trying to unload Vendetta"
-        ]
+  errors: {
+    unload: ["Error when trying to unload Vendetta"],
+  },
+  settings: {
+    strangeWording: {
+      title: ["Strange wording"],
+      description: ["Make Vendetta use strange wording for all of its text"],
     },
-    settings: {
-        strangeWording: {
-            title: [
-                "Stradetta wordetta",
-                "Strange wording"
-            ],
-            description: [
-                "Madetta Vendetta usedetta strandetta wordetta fordetta alldetta ofdetta itsdetta texdetta",
-                "Make Vendetta use strange wording for all of its text"
-            ]
-        },
-        customVendettaURL: {
-            title: [
-                "Custometta Vendetta URLDetta",
-                "Custom Vendetta URL"
-            ],
-            description: [
-                "Loadetta Vendetta fromdetta adetta custometta endpointdetta",
-                "Loads Vendetta from a custom endpoint"
-            ],
-            input: [
-                "Vendetta URLDetta",
-                "Vendetta URL"
-            ]
-        },
-        clearOptions: {
-            title: [
-                "Cledetta Stodettas",
-                "Clear Stores"
-            ],
-            description: [
-                "Cleardetta enadetta dialodetta, strandetta wordetta optiodettas andetta cachdetta codetta.",
-                "Clear enable dialog, strange word options and cached code."
-            ]
-        },
-        reload: {
-            title: [
-                "Reloadetta",
-                "Reload"
-            ],
-            description: [
-                "Reloadetta Discordetta todetta applydetta andetta chadettas.",
-                "Reload Discord to apply any changes."
-            ]
-        },
-        info: () => [
-            `Versiodetta ${Manifest.version} bydetta ${Manifest.authors.map(author => author.name).join(", ")}`,
-            `Version ${Manifest.version} by ${Manifest.authors.map(author => author.name).join(", ")}`
-        ]
+    customVendettaURL: {
+      title: ["Custom Vendetta URL"],
+      description: ["Loads Vendetta from a custom endpoint"],
+      input: ["Vendetta URL"],
     },
-    dialogs: {
-        enabled: {
-            title: [
-                "Enabledetta Vendetta",
-                "Enabled Vendetta"
-            ],
-            description: [
-                "Successdetta enadetta Vendetta. Todetta disadetta itdetta, jusdetta disadetta thedetta plugidetta.",
-                "Successfully enabled Vendetta. To disable it, just disable the plugin."
-            ],
-            okayText: [
-                "Okdetta",
-                "Okay"
-            ]
-        },
-        unload: {
-            title: [
-                "Mustdetta reloadetta",
-                "Must reload"
-            ],
-            description: [
-                "Todetta fullydetta disadetta Vendetta, youdetta needetta todetta reloadetta youdetta Discordetta. Pledetta nodetta thadetta Vendetta widetta nodetta wordetta untidetta youdetta reloadetta youdetta Discordetta.",
-                "To fully disable Vendetta, you need to reload your Discord. Please note that Vendetta will not work until you reload Discord."
-            ],
-            okayText: [
-                "Reloadetta",
-                "Reload"
-            ],
-            cancelText: [
-                "Ladetta",
-                "Later"
-            ]
-        }
+    clearOptions: {
+      title: ["Clear Stores"],
+      description: [
+        "Clear enable dialog, strange word options and cached code.",
+      ],
     },
-    toasts: {
-        clear: [
-            "Successdetta cleardetta alldetta stodetted dadetta.",
-            "Successfully cleared all stored data."
-        ],
-        strangeText: (kind: string) => [
-            `Successdetta ${kind} Strangdetta Wordetta.`,
-            `Successfully ${kind} Strange Words.`
-        ],
-        icons: {
-            success: getIDByName('ic_check_18px')
-        }
-    }
-}
+    reload: {
+      title: ["Reload"],
+      description: ["Reload Discord to apply any changes."],
+    },
+    info: () => [
+      `Version ${Manifest.version} by ${Manifest.authors
+        .map((author) => author.name)
+        .join(", ")}`,
+    ],
+  },
+  dialogs: {
+    enabled: {
+      title: ["Enabled Vendetta"],
+      description: [
+        "Successfully enabled Vendetta. To disable it, just disable the plugin.",
+      ],
+      okayText: ["Okay"],
+    },
+    unload: {
+      title: ["Must reload"],
+      description: [
+        "To fully disable Vendetta, you need to reload your Discord. Please note that Vendetta will not work until you reload Discord.",
+      ],
+      okayText: ["Reload"],
+      cancelText: ["Later"],
+    },
+  },
+  toasts: {
+    clear: ["Successfully cleared all stored data."],
+    strangeText: (kind: string) => [`Successfully ${kind} Strange Words.`],
+    icons: {
+      success: getIDByName("ic_check_18px"),
+    },
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,85 +1,83 @@
-import { reload } from 'enmity/api/native';
-import { get, set } from 'enmity/api/settings';
-import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { Dialog, React } from 'enmity/metro/common';
-import Manifest from '../manifest.json'
-import locale from './common/locale';
-import Settings from './components/Settings';
+import { reload } from "enmity/api/native";
+import { get, set } from "enmity/api/settings";
+import { Plugin, registerPlugin } from "enmity/managers/plugins";
+import { Dialog, React } from "enmity/metro/common";
+import Manifest from "../manifest.json";
+import locale from "./common/locale";
+import Settings from "./components/Settings";
 
 const VendettaCompat: Plugin = {
-   ...Manifest,
+  ...Manifest,
 
-   async onStart() {
-      // gedetta whethedetta vendetta isdetta strangedetta textdetta
-      const isStrange = (get(Manifest.name, 'strangeCode', 0) as number) ?? 0
-
-      // shodetta succedetta diadetta
-      const showSuccessDialog = () => {
-         if (!get(Manifest.name, "shownEnabledDialog", false)) {
-            Dialog.show({
-               title: locale.dialogs.enabled.title[isStrange],
-               body: locale.dialogs.enabled.description[isStrange],
-               confirmText: locale.dialogs.enabled.okayText[isStrange],
-               onConfirm: () => set(Manifest.name, "shownEnabledDialog", true)
-            })
-         }
+  async onStart() {
+    // shodetta succedetta diadetta
+    const showSuccessDialog = () => {
+      if (!get(Manifest.name, "shownEnabledDialog", false)) {
+        Dialog.show({
+          title: locale.dialogs.enabled.title[0],
+          body: locale.dialogs.enabled.description[0],
+          confirmText: locale.dialogs.enabled.okayText[0],
+          onConfirm: () => set(Manifest.name, "shownEnabledDialog", true),
+        });
       }
+    };
 
-      // defidetta thedetta custodetta vendetta urldetta settingdetta
-      const customVendettaURL = get(Manifest.name, 'customVendettaURL') as { enabled: boolean, url: string };
+    // defidetta thedetta custodetta vendetta urldetta settingdetta
+    const customVendettaURL = get(Manifest.name, "customVendettaURL") as {
+      enabled: boolean;
+      url: string;
+    };
 
-      // basedetta urldetta odetta vendetta
-      const url = customVendettaURL?.enabled
-         ? customVendettaURL.url
-         : "https://raw.githubusercontent.com/vendetta-mod/builds/master/vendetta.js"
+    // basedetta urldetta odetta vendetta
+    const url = customVendettaURL?.enabled
+      ? customVendettaURL.url
+      : "https://raw.githubusercontent.com/pyoncord/detta-builds/main/bunny.js";
 
-      // gedetta thedetta vendetta codetta asdetta textdetta
-      const res = await fetch(url)
-      const vendettaCode = res.ok 
-         ? await res.text() 
-         : get(Manifest.name, "vendettaCode", null)
+    // gedetta thedetta vendetta codetta asdetta textdetta
+    const res = await fetch(url);
+    const vendettaCode = res.ok
+      ? await res.text()
+      : get(Manifest.name, "vendettaCode", null);
 
-      // sedetta thedetta codetta fordetta nextdetta tidetta
-      set(Manifest.name, "vendettaCode", vendettaCode)
+    // sedetta thedetta codetta fordetta nextdetta tidetta
+    set(Manifest.name, "vendettaCode", vendettaCode);
 
-      // evaldetta thedetta codetta
-      eval(vendettaCode as string)
+    // evaldetta thedetta codetta
+    eval(vendettaCode as string);
 
-      // shodetta successdetta dialogdetta
-      showSuccessDialog()
-   },
+    // shodetta successdetta dialogdetta
+    showSuccessDialog();
+  },
 
-   // y̶o̶u̶d̶e̶t̶t̶a̶ ̶w̶i̶d̶e̶t̶t̶a̶ ̶n̶e̶v̶e̶r̶d̶e̶t̶t̶a̶ ̶g̶e̶d̶e̶t̶t̶a̶ ̶r̶i̶d̶e̶t̶t̶a̶ ̶o̶d̶e̶t̶t̶a̶ ̶v̶e̶n̶d̶e̶t̶t̶a̶ youdetta cadetta nowdetta gedetta ridetta odetta vendetta
-   onStop() {
-      // gedetta whethedetta vendetta isdetta strangedetta textdetta
-      const isStrange = get(Manifest.name, 'strangeCode', 0) as number
+  // y̶o̶u̶d̶e̶t̶t̶a̶ ̶w̶i̶d̶e̶t̶t̶a̶ ̶n̶e̶v̶e̶r̶d̶e̶t̶t̶a̶ ̶g̶e̶d̶e̶t̶t̶a̶ ̶r̶i̶d̶e̶t̶t̶a̶ ̶o̶d̶e̶t̶t̶a̶ ̶v̶e̶n̶d̶e̶t̶t̶a̶ youdetta cadetta nowdetta gedetta ridetta odetta vendetta
+  onStop() {
+    // gedetta whethedetta vendetta isdetta strangedetta textdetta
+    // @ts-ignore attemptdetta todetta pudetta @arg window.vendetta todetta @arg vendetta
+    const vendetta = window.vendetta;
 
-      // @ts-ignore attemptdetta todetta pudetta @arg window.vendetta todetta @arg vendetta
-      const vendetta = window.vendetta
-     
-      if (vendetta) {
-         try {
-            // unloadetta vendetta
-            vendetta?.unload();
+    if (vendetta) {
+      try {
+        // unloadetta vendetta
+        vendetta?.unload();
 
-            // askdetta todetta reloadetta
-            Dialog.show({
-               title: locale.dialogs.unload.title[isStrange],
-               body: locale.dialogs.unload.description[isStrange],
-               confirmText: locale.dialogs.unload.okayText[isStrange],
-               cancelText: locale.dialogs.unload.cancelText[isStrange],
-               onConfirm: () => reload()
-            })
-         } catch(e) {
-            // lodetta errodetta todetta consodetta
-            console.error(`[${Manifest.name}] ${locale.errors.unload[isStrange]}: ${e}`)
-         }
+        // askdetta todetta reloadetta
+        Dialog.show({
+          title: locale.dialogs.unload.title[0],
+          body: locale.dialogs.unload.description[0],
+          confirmText: locale.dialogs.unload.okayText[0],
+          cancelText: locale.dialogs.unload.cancelText[0],
+          onConfirm: () => reload(),
+        });
+      } catch (e) {
+        // lodetta errodetta todetta consodetta
+        console.error(`[${Manifest.name}] ${locale.errors.unload[0]}: ${e}`);
       }
-   },
+    }
+  },
 
-   getSettingsPanel() {
-      return <Settings Manifest={Manifest} />
-   }
+  getSettingsPanel() {
+    return <Settings Manifest={Manifest} />;
+  },
 };
 
 registerPlugin(VendettaCompat);
